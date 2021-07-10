b2a24d77e25ce72caff4a06ce1d1f1ea198774e5
Maria <dziuba.m.o@yandex.ru>
2021-07-10 12:48:05 +0500

    add hw10 web

---
+++ WEB/hw10/src/components/middle/Index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <MiddlePost v-for="post in allPosts"
+                    :post="post" :users="users" :comments="ourComment(post)" :key="post.id"/>
+    </div>
+</template>
+
+<script>
+    import MiddlePost from "./MiddlePost";
+    export default {
+        name: "Index",
+        components: {MiddlePost},
+        props: ['users', 'posts', 'comments'],
+        computed: {
+            allPosts: function() {
+                return Object.values(this.posts).sort((p1, p2) => p2.id - p1.id);
+            }
+        },
+        methods: {
+            ourComment: function(post) {
+                return Object.values(this.comments).filter(comment => comment.postId ===post.id);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
